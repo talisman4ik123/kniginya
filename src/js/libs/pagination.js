ebb1a89c5fb3adc944397d224c8a0442fe7c436b
Dmitriy <zaitsev.d.e.2000@gmail.com>
2022-01-05 03:08:57 +0300

    add section contacts and discounts

--- src/js/libs/pagination.js
+++ src/js/libs/pagination.js
@@ -1,31 +1,31 @@
-function createPageNav (pageCount, list) {
+function createPageNav(pageCount, list) {
     const listWrapper = document.querySelector(list).parentElement;
     listWrapper.innerHTML += "<div class = 'page-group'> <ul class = 'page-group__list'></ul> </div>";
     const pageList = document.querySelector('.page-group__list');
     for (let i = 0; i < pageCount; i++) {
         if (i == 0) {
-            pageList.innerHTML += `<li class = 'page-group__item page-group__item--active'> <a class = "page-group__link">${i+1}</a> </li>`;
+            pageList.innerHTML += `<li class = 'page-group__item page-group__item--active'> <a class = "page-group__link">${i + 1}</a> </li>`;
             continue;
         }
-        pageList.innerHTML += `<li class = 'page-group__item'> <a class = "page-group__link">${i+1}</a> </li>`;
+        pageList.innerHTML += `<li class = 'page-group__item'> <a class = "page-group__link">${i + 1}</a> </li>`;
     }
 }
 
-function createPage (pageCount, list) {
+function createPage(pageCount, list) {
     const listWrapper = document.querySelector(list).parentElement;
     list = list.split('.').join('');
-    for (let i=0; i < pageCount-1; i++) {
+    for (let i = 0; i < pageCount - 1; i++) {
         listWrapper.innerHTML += `<ul class = ${list}></ul>`;
     }
 }
 
-function addItemInPage (list, items, itemCount) {
+function addItemInPage(list, items, itemCount) {
     const lists = document.querySelectorAll(list);
     const arrayList = Array.from(items);
-    for (let i=0; i<lists.length; i++) {
+    for (let i = 0; i < lists.length; i++) {
         let count = 0;
         if (itemCount < arrayList.length) {
-            for (let k=0; k<itemCount; k++) {
+            for (let k = 0; k < itemCount; k++) {
                 let temp = 0;
                 lists[i].append(arrayList[temp]);
                 count++;
@@ -36,7 +36,7 @@ function addItemInPage (list, items, itemCount) {
             continue;
         }
         if (itemCount >= arrayList.length && arrayList.length != 0) {
-            for (let k=0; k<arrayList.length; k++) {
+            for (let k = 0; k < arrayList.length; k++) {
                 let temp = 0;
                 lists[i].append(arrayList[temp]);
                 arrayList.shift();
@@ -49,14 +49,14 @@ function clickBtn(list) {
     const btns = document.querySelectorAll('.page-group__link');
     btns.forEach(btn => {
         btn.addEventListener("click", () => {
-            for (let i=0; i<btns.length; i++) {
+            for (let i = 0; i < btns.length; i++) {
                 if (btns[i].parentElement.classList.contains("page-group__item--active")) {
                     btns[i].parentElement.classList.remove("page-group__item--active");
                 }
             }
             const lists = document.querySelectorAll(list);
             const listName = list.split('.').join('');
-            for (let i=0; i<lists.length; i++) {
+            for (let i = 0; i < lists.length; i++) {
                 if (lists[i].classList.contains(`${listName}--active`)) {
                     lists[i].classList.remove(`${listName}--active`);
                 }
@@ -64,14 +64,18 @@ function clickBtn(list) {
 
             btn.parentElement.classList.add("page-group__item--active");
             let count = 0;
-            for (let i=0; i<btns.length; i++) {
+            for (let i = 0; i < btns.length; i++) {
                 if (btns[i].parentElement.classList.contains("page-group__item--active")) {
                     count = i;
                 }
             }
-            for (let i=0; i<lists.length; i++) {
-                if (i==count) {
+            for (let i = 0; i < lists.length; i++) {
+                if (i == count) {
                     lists[i].classList.add(`${listName}--active`);
+                    window.scrollTo({
+                        top: document.querySelector(".discounts__search-box").offsetHeight,
+                        behavior: "smooth"
+                    });
                 }
             }
             console.log(document.documentElement.scrollHeight);
@@ -79,7 +83,7 @@ function clickBtn(list) {
     });
 }
 
-function pagination (list, item, itemCount) {
+export function pagination(list, item, itemCount) {
     const items = document.querySelectorAll(item);
     document.querySelector(list).classList.add(`${list.split(".").join("")}--active`);
     if (itemCount < items.length) {
--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,106 @@ isWebp();
 import { burger } from "./libs/burger.js";
 burger('burger-content');
 
+if (document.title == "Контакты") {
+    const form = document.querySelector("#contact-form");
+    form.addEventListener("input", inputHandler);
+    const inputArr = Array.from(form);
+    const validIputArr = [];
+    inputArr.forEach(element => {
+        if (element.hasAttribute("data-reg")) {
+            element.setAttribute("is-valid", "0");
+            validIputArr.push(element);
+        }
+    });
+
+    function buttonHandler(e) {
+        const isAllValid = [];
+        validIputArr.forEach((el) => {
+            isAllValid.push(el.getAttribute("is-valid"));
+        });
+        let temp = 0;
+        for (let i = 0; i < isAllValid.length; i++) {
+            if (isAllValid[i] == 0) {
+                temp++;
+            }
+        }
+        if (temp == 0) {
+            const checkbox = document.querySelector('#form-write-to-us-checkbox');
+            if (checkbox.checked) {
+                const capture = document.querySelector(".form-write-to-us__capture");
+                const captureSrc = capture.getAttribute("src");
+                const captureNumber = parseInt(captureSrc.match(/\d+/)) - 1;
+                const inputCaptureValue = document.querySelector("#input-capture").value;
+                if (inputCaptureValue !== captureArr[captureNumber]) {
+                    e.preventDefault();
+                    alert("Капча введена неправильно!");
+                }
+            } else {
+                document.querySelector(".checkbox__text").style.color = "red";
+                e.preventDefault();
+            }
+        } else {
+            e.preventDefault();
+        }
+    }
+
+    const submitBtn = document.querySelector(".form-write-to-us__btn");
+    submitBtn.addEventListener("click", buttonHandler);
+
+    function inputHandler({ target }) {
+        const toolTip = target.previousElementSibling;
+        if (target.hasAttribute("data-reg")) {
+            inputCheck(target, toolTip);
+        }
+    }
+
+    function inputCheck(el, toolTip) {
+        const inputValue = el.value;
+        const inputReg = el.getAttribute("data-reg");
+        const inputName = el.getAttribute("name");
+        const reg = new RegExp(inputReg);
+        if (reg.test(inputValue)) {
+            el.style.border = "1px solid rgb(0, 196, 0)";
+            el.setAttribute("is-valid", "1");
+            toolTip.classList.remove("active");
+
+        } else {
+            el.style.border = "1px solid rgb(255, 0, 0)";
+            el.setAttribute("is-valid", "0");
+            toolTip.classList.add("active");
+        }
+        if (inputName == "name" && inputValue.length < 2) {
+            el.style.border = "1px solid rgb(255, 0, 0)";
+            el.setAttribute("is-valid", "0");
+            toolTip.classList.add("active");
+        }
+    }
+    const captureArr = ["3blHDas", "75NP2DW", "B2v7KLp", "dTaFkWk", "m5r2bCz", "pRbwf3L", "WRZSR5B", "z2bD6Pe", "uZrTGTv", "rmUMpgF"];
+    const refreshBtn = document.querySelector(".svg-refresh");
+    refreshBtn.addEventListener("click", refreshCapture);
+
+    function refreshCapture() {
+        const capture = refreshBtn.previousElementSibling;
+        let captureSrc = capture.getAttribute("src");
+        let captureNumber = parseInt(captureSrc.match(/\d+/));
+        let newCaptureNumber = String(captureNumber + 1);
+        captureNumber = String(captureNumber);
+        if (newCaptureNumber == '11') {
+            newCaptureNumber = '1';
+        }
+        captureSrc = captureSrc.replace(captureNumber, newCaptureNumber);
+        capture.setAttribute("src", captureSrc);
+        capture.style.width = "100px";
+    }
+}
+
+
+
+
+// import "./libs/inputMask.js";
+// const inputmask = window.Inputmask;
+
+
 // import {formPopup} from "./libs/popupForm.js"
 // formPopup(openBtns, fromBG, form, formBody);
 
@@ -20,9 +120,46 @@ burger('burger-content');
 // }
 
 
-//import {pagination} from "./libs/pagination.js";
-//pagination(list, item, itemCount);
-
-
 //import Swiper, { Navigation, Pagination } from 'swiper';
 //const swiper = new Swiper();
+
+function openSelect(selectClass) {
+    const select = document.querySelector(`.${selectClass}`);
+    select.addEventListener("click", () => {
+        select.classList.toggle(`${selectClass}--active`);
+    });
+
+    document.addEventListener('click', function (event) {
+        if (!select.contains(event.target)) {
+            select.classList.remove(`${selectClass}--active`);
+        }
+    });
+}
+
+function selectItem(selectableItem) {
+    const allItemSelect = document.querySelectorAll(`.${selectableItem}`);
+    allItemSelect[0].classList.add(`${selectableItem}--active`);
+    for (let i = 0; i < allItemSelect.length; i++) {
+        allItemSelect[i].setAttribute("order-attr", i);
+        allItemSelect[i].addEventListener("click", () => {
+            const clickItemAttr = allItemSelect[i].getAttribute("order-attr");
+            const activeItem = document.querySelector(`.${selectableItem}--active`);
+            activeItem.setAttribute("order-attr", clickItemAttr);
+            activeItem.style.order = clickItemAttr;
+            activeItem.classList.remove(`${selectableItem}--active`);
+            allItemSelect[i].setAttribute("order-attr", 0);
+            allItemSelect[i].style.order = 0;
+            allItemSelect[i].classList.add(`${selectableItem}--active`);
+        });
+    }
+}
+
+if (document.title == "Акции") {
+    openSelect("sort-filters__list");
+    selectItem("sort-filters__item");
+    openSelect("items-filters__list");
+    selectItem("items-filters__item");
+}
+
+import { pagination } from "./libs/pagination.js";
+pagination(".discounts__list", ".discounts__item", 10);
